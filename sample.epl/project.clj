537d4e229ff735ed3d2204037256a7c394778b62
Kei Suzuki <ksdevx@gmail.com>
2011-02-18 15:12:23 +0900

    Update samples

--- sample.epl/project.clj
+++ sample.epl/project.clj
@@ -2,7 +2,7 @@
   :description "Doctor regeneration project"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[org.clojars.ksuzuki/lein-lic "0.1.0"]]
+  :dev-dependencies [[lein-lic "1.0.0"]]
   :lic {:artifact "dregen - Doctor regenerator"
         :author ["Romana"]}
   :namespaces [dregen
